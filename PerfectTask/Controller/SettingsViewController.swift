90994686322ae9ae73e98ebeb89aa89ba753802a
ahmednasr8898 <ans96283@gmail.com>
2021-03-01 22:45:42 +0200

    SettingsPageDesign

---
+++ PerfectTask/Controller/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  PerfectTask
+//
+//  Created by Ahmed Nasr on 3/1/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
